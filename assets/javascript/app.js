9f8116814a69697710b03bdc8c60fc2432be6cb7
km <42688915+ksemal@users.noreply.github.com>
2018-11-11 14:03:15 -0500

    set the question object

---
+++ assets/javascript/app.js
@@ -0,0 +1,107 @@
+var myQuestions = [
+    {
+        question: "How do crickets hear?",
+        answerA: "Through their wings",
+        answerB: "Through their belly",
+        answerC: "Through their knees",
+        answerD: "Through their tongue",
+        correctAnswer: "c"
+    },
+    {
+        question: "Which American city invented plastic vomit?",
+        answerA: "Chicago",
+        answerB: "Detroit",
+        answerC: "Columbus",
+        answerD: "Baltimore",
+        correctAnswer: "a"
+    },
+    {
+        question: "What was Karl Marx’s favorite color?",
+        answerA: "Brown",
+        answerB: "Blue",
+        answerC: "Red",
+        answerD: "Purple",
+        correctAnswer: "c"
+    },
+    {
+        question: "What’s the best way to stop crying while peeling onions?",
+        answerA: "Lick almonds",
+        answerB: "Suck lemons",
+        answerC: "Eat cheese",
+        answerD: "Chew gum",
+        correctAnswer: "d"
+    },
+    {
+        question: "Which animal sleeps for only five minutes a day?",
+        answerA: "A chameleon",
+        answerB: "A koala",
+        answerC: "A giraffe",
+        answerD: "A beaver",
+        correctAnswer: "c"
+    },
+    {
+        question: "One human hair can support how many kilograms?",
+        answerA: "Three",
+        answerB: "Five",
+        answerC: "Seven",
+        answerD: "Nine",
+        correctAnswer: "a"
+    },
+    {
+        question: "Which European city is home to the Fairy Investigation Society?",
+        answerA: "Poznan",
+        answerB: "Dublin",
+        answerC: "Bratislava",
+        answerD: "Tallinn",
+        correctAnswer: "b"
+    },
+    {
+        question: "What’s a frog’s favorite color?",
+        answerA: "Blue",
+        answerB: "Orange",
+        answerC: "Yellow",
+        answerD: "Brown",
+        correctAnswer: "a"
+    },
+    {
+        question: "Which one of these planets rotates clockwise?",
+        answerA: "Uranus",
+        answerB: "Mercury",
+        answerC: "Pluto",
+        answerD: "Venus",
+        correctAnswer: "d"
+    },
+    {
+        question: "Which country leads the world in cork production?",
+        answerA: "Greece",
+        answerB: "Australia",
+        answerC: "Spain",
+        answerD: "Mexico",
+        correctAnswer: "c"
+    },
+    {
+        question: "What color was Coca-Cola originally?",
+        answerA: "Red",
+        answerB: "Purple",
+        answerC: "Beige",
+        answerD: "Green",
+        correctAnswer: "d"
+    },
+    {
+        question: "Bubble gum contains what?",
+        answerA: "Plastic",
+        answerB: "Calcium",
+        answerC: "Rubber",
+        answerD: "Pepper",
+        correctAnswer: "c"
+    },
+    {
+        question: "The inventor of the paint roller was of which nationality?",
+        answerA: "Hungarian",
+        answerB: "Canadian",
+        answerC: "Norwegian",
+        answerD: "Argentinian",
+        correctAnswer: "b"
+    }
+  ];
+
